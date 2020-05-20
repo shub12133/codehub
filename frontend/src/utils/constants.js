82838ec28067e884d43857336425f313dfddd851
bharathreddyza <bharathreddyza@gmail.com>
2020-05-20 17:00:21 +0530

    added route sidebar

---
+++ frontend/src/utils/constants.js
@@ -0,0 +1,9 @@
+let url  = '';
+
+if(process.env.NODE_ENV === 'development'){
+    url = "http://localhost:3001/api/"
+}else if(process.env.NODE_ENV === 'production'){
+    url = "https://domain.com/api"
+}
+
+export const host = url
