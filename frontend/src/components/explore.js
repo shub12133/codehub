4fe95ad5bc6e263222ec52bd258abb931f45689b
shub12133 <shub.123433@gmail.com>
2020-06-17 08:17:05 +0530

    Enter

--- frontend/src/components/explore.js
+++ frontend/src/components/explore.js
@@ -6,7 +6,10 @@ import StarBorderIcon from '@material-ui/icons/StarBorder';
 import BuildIcon from '@material-ui/icons/Build';
 import {connect} from 'react-redux'
 import {getProjects,getUser} from '../actions/gitActions'
+<<<<<<< HEAD
 // import useState from '../commonCss/CommonCss'
+=======
+>>>>>>> f386460f21effa76d0d3f5f5596e6a5ac8d65b8e
 const api = new Gitlab({
     host: `https://codehub.code.in`,
     token: 'cBsT5RSaeRYwDGiBL9iL',
@@ -17,7 +20,12 @@ const api = new Gitlab({
 
  
   function Explore(props) {
+<<<<<<< HEAD
     const {getProjects,projects,user,getUser} = props
+=======
+   
+const {getProjects,projects,user,getUser} = props
+>>>>>>> f386460f21effa76d0d3f5f5596e6a5ac8d65b8e
 
     const [state, setState] = useState({
     projects:[],
@@ -26,6 +34,7 @@ const api = new Gitlab({
 
 
  useEffect(()=> {
+<<<<<<< HEAD
   getProjects()
   .then((projects)=>{
     setState({
@@ -39,7 +48,23 @@ const api = new Gitlab({
  
   getUser(user.username)
 
+=======
+  
 
+  getProjects()
+  .then((projects)=>{
+    setState({
+      ...state,
+      projects:projects
+    })
+  }).catch((err)=>{
+    console.log(err)
+  })
+  getUser(user.user.username)
+ 
+>>>>>>> f386460f21effa76d0d3f5f5596e6a5ac8d65b8e
+
+ 
   },[])
 
     return (
@@ -63,7 +88,11 @@ const api = new Gitlab({
 
 const mapStateToProps=(state)=>({
   projects:state.projects.projects,
+<<<<<<< HEAD
   user:state.auth.user,
+=======
+  user:state.auth,
+>>>>>>> f386460f21effa76d0d3f5f5596e6a5ac8d65b8e
   
 })
 
